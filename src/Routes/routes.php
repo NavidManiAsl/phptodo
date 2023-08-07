35f94e4d7fe7b0d79f43841272ce06019a72efcb
NavidManiasl <navid.maniasl@outlook.com>
2023-08-07 22:27:23 +0200

    fix the routing

--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -2,8 +2,28 @@
 
 use Slim\Routing\RouteCollectorProxy;
 require __DIR__ . '/../bootstrap.php';
-// Define your routes using the $app instance
+require __DIR__ . '/../Controllers/TodoController.php';
+
+
+
 $app->get('/', function($request, $response) {
     $response->getBody()->write('Hello, world!');
     return $response;
 });
+$app->group('/tasks', function (RouteCollectorProxy $group) {
+
+    $todoController = new TodoController(new TodoRepository(new Database(
+        $_ENV['POSTGRES_PORT'],
+        $_ENV['POSTGRES_DB'],
+        $_ENV['POSTGRES_USER'],
+        $_ENV['POSTGRES_PASSWORD']
+    )));
+
+    $group->get('', [$todoController, 'getAllTasks']);
+    $group->get('/{id}', [$todoController, 'getTask']);
+    $group->post('', [$todoController, 'addTask']);
+    $group->delete('/{id}', [$todoController, 'deleteTask']);
+    $group->put('/{id}', [$todoController, 'editTask']);
+    $group->put('/{id}/toggle', [$todoController, 'taskStatusToggle']);
+});
+
