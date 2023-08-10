c0878deef6824733101da98f803d8ec545699e6e
NavidManiasl <navid.maniasl@outlook.com>
2023-08-11 01:17:23 +0200

    Cors added

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -13,4 +13,17 @@ $dotenv = Dotenv::createImmutable(__DIR__.'/../');
 $dotenv->load();
 
 $app = AppFactory::create();
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response
+    ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+    ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+    ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+    ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 $app->addBodyParsingMiddleware();
